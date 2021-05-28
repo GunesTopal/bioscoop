fb79d02fbe317cc4a7705b94004c628a70153f99
Günes Topalm <gunes.topal@gmail.com>
2021-05-28 23:10:42 +0200

    Proberen om de films in de database bij klasse Film te plakken

---
+++ models/clsFilm.py
@@ -0,0 +1,20 @@
+
+
+class Film():
+    def __init__(self, ID_Film, Titel, Duur, DrieD_beschikbaar, KNT, IMDB_ID, Poster_link, Beschrijving):
+        self._ID_Film = ID_Film
+        self._Titel = Titel
+        self.Duur = Duur
+        self.DrieD_beschikbaar = DrieD_beschikbaar
+        self.KNT = KNT
+        self.IMDB_ID = IMDB_ID
+        self.Poster_link = Poster_link
+        self.Beschrijving = Beschrijving
+
+    def __str__(self):
+        return f"{self._Titel}"
+
+    @property
+    def ID_Film(self):
+        return self._ID_Film
+
