13f35be336bf52f80dba58495d426e7ff495ef47
Günes Topalm <gunes.topal@gmail.com>
2021-06-22 13:05:07 +0200

    laatste push

--- kiosk-pro.py
+++ kiosk-pro.py
@@ -1,5 +1,9 @@
-from subprocess import run
+# from subprocess import run
+from re import A, I
 import PySimpleGUI as gui
+from PIL import Image
+import os, io
+import urllib.request
 import sqlite3
 from datetime import datetime
 from time import sleep
@@ -10,33 +14,7 @@ from tools.movie_request import vind_film_via_imdb
 # Documentatie: https://pysimplegui.readthedocs.io/en/latest/call%20reference/
 
 # ------------------ MISC ----------------
-gui.theme('LightBrown8')
-
-# ------------------ CUSTOM VARIABLES ----------------
-vandaag=datetime.now()
-vandaag_datum='06/06/2021'#vandaag.strftime("%d/%m/%Y")
-vandaag_tijd=vandaag.strftime("%H:%M")
-vandaag_jaar=vandaag.strftime("%Y")
-vandaag_maand_getal=vandaag.strftime("%M")
-vandaag_week=vandaag.strftime("%W")
-prijs_ticket={'standaard':9,'kids':7,'langspeel':1,'3D':1.5}
-
-allevertoningen=get_vertoning_list() #vb: [7, (1, 'Forrest Gump'), '04/06/2021', '10:00', 'Nee', 7]
-alleverkopen=get_verkoop_list() #vb: [1, 1, 2, 2, 32.0]
-allefilms=get_inventory_list() #vb: [18, 'Alien', 122, 'Ja', 'tt2316204', 'Nee']
-
-titel=[""]
-datum=""
-moment=""
-zaal=""
-DrieD=""
-
-#TODO:check variabel
-huidigevertoning=[vertoning for vertoning in allevertoningen if vertoning[2]=="06/06/2021"]#vandaag_datum] vb: [7, (1, 'Forrest Gump'), '04/06/2021', '10:00', 'Nee', 7]
-lijstfilms=[(vertoning[1][0],vertoning[1][1]) for vertoning in huidigevertoning] #vb:[(18, 'Alien')]
-gegevensfilms=[(f[0], f[1], k[2],k[3],k[4]) for k in allefilms for f in lijstfilms if k[0] == f[0] ] #vb: [(18, 'Alien', 122, 'Ja', tt2316204)]
-films=[i[1] for i in lijstfilms]
-
+gui.theme('LightBlue4')
 
 
 # ------------------ CUSTOM FUNCTIONS ----------------
@@ -59,177 +37,194 @@ def insert_verkoop_database(ID_Vertoning, Tickets_kids, Tickets_standaard, Prijs
     con.commit()
     con.close()
 
+def lijsturl_naar_lijstimagegui(lijst, x=150, y=150, sleutel="POSTER"): #neemt de lijst met (meerdere) URL's, steekt die in een ander lijst als GUIimage object en geeft het een automatische key met nummer
+    #lijst vb: [14, 'Back to the Future', 116, 'Nee', 'https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg', 'tt0088763']
+    posters=[]
+    if len(lijst) > 1:
+        for item in lijst:
+            urllib.request.urlretrieve(item[4],"URL")
+            image = Image.open("URL")
+            image.thumbnail((x, y))
+            bio = io.BytesIO()
+            image.save(bio, format="PNG")
+            gegevens=bio.getvalue()
+            posters.append([gui.Image(data=gegevens, key=f"-{sleutel}{lijst.index(item)+1}-", enable_events=True)])
+    else:
+            urllib.request.urlretrieve(lijst[0][4],"URL")
+            image = Image.open("URL")
+            image.thumbnail((x, y))
+            bio = io.BytesIO()
+            image.save(bio, format="PNG")
+            gegevens=bio.getvalue()
+            posters.append([gui.Image(data=gegevens)])#, key=f"-{key}-", enable_events=True)])
+    return posters 
+
 # ------------------ WINDOW 0 ----------------
 def win0():
     layout0=[
-        [gui.Image('data/splash.png')]
+        [gui.Image('data/splash.png')],
+        [gui.Button('', key='-LADEN-', visible=False)]
     ]
 
-    window0= gui.Window("",layout0, background_color='black',margins = (0, 0), no_titlebar=True,size=(347,196))
+    window0= gui.Window('Window Title', [[gui.Image('data/splash.png')]], transparent_color=gui.theme_background_color(), no_titlebar=True, keep_on_top=True)
     return window0
 
+
 # ------------------ WINDOW 1 ----------------
-def win1():
-    kolom3=[
-        [gui.Text('Aantal Volwassenen:')],
-        [gui.Text('Aantal Kinderen:', key='-AANTAL_KINDEREN-')],
-        [gui.Text('Prijs')],
-        [gui.Button('Tickets kopen', key='-KNOP-', enable_events=True, disabled=True)]
-    ]
-    kolom4=[
-        [gui.Spin([str(i) for i in range(11)], key='-SPINVOLWASSENEN-', initial_value="0",enable_events=True)],
-        [gui.Spin([str(i) for i in range(11)], key='-SPINKINDEREN-',initial_value="0", enable_events=True)],
-        [gui.Text('', size=(5,1),key='-PRIJS-', enable_events=True)]
-    ]
-    kolom1=[
-        [gui.Listbox(films, size=(38, 13), key="-films-", enable_events=True)]
-    ]
-    kolom2=[
-        [gui.Text('Vertoningen')],
-        [gui.Listbox("", size=(38, 5), key="-infofilm-", enable_events=True)],
-        [gui.Column(kolom3, vertical_alignment="top"),gui.Column(kolom4,vertical_alignment="top")]
-    ]
-    kolom5=[
-        [gui.Image('data/logo_cinemax.png')]
-    ]
+def win1(lijst): #rijen met max 4 posters naast elkaar
+    #vb: lijst= [[14, 'Back to the Future', 116, 'Nee', 'https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg', 'tt0088763'],...]
+    rij1=[]
+    rij2=[]
+    rij3=[]
+    rij4=[]
+    images=lijst
+    for i in images:
+        if images.index(i)<4:
+            rij1+=i
+        elif images.index(i)<8 and images.index(i)>3:
+            rij2+=i
+        elif images.index(i)<12 and images.index(i)>7:
+            rij3+=i
+        elif images.index(i)<16 and images.index(i)>11:
+            rij4+=i
 
-    kolom8=[
-        [gui.Text("",size=(8,1),key='-DUUR-')]
+    layout=[
+        [gui.Image('data/logo_cinemax.png')],
+        [gui.Column([rij1])],
+        [gui.Column([rij2])],
+        [gui.Column([rij3])],
+        [gui.Column([rij4])]
     ]
+    window = gui.Window("Cinemax", layout,size=(550, 650), element_justification = "center")
+    return window
 
-    kolom9=[
-        [gui.Text("KINDEREN NIET TOEGELATEN",key='-KNT-',text_color="green", enable_events=True)]
-    ]
+# ------------------ WINDOW 2 ----------------#TODO: hier is something wrong
+def win2(lijst): # lijst vb: [14, 'Back to the Future', 116, 'Nee', 'https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg', 'tt0088763']
+    info=[]
+    titel=lijst[1]
+    for i in allevertoningen:
+        if i[1][1]==titel and i[2]==vandaag_datum:
+            vertoningid=i[0]
+            datum=i[2]
+            moment=i[3]
+            zaal=i[5]
+            DrieD='.' if i[4]=='Nee' else ' in 3D.'
+            info.append(f"{datum} om {moment} uur in zaal {zaal}{DrieD}")
 
-    kolom7=[
-        [gui.Column(kolom8, vertical_alignment="top", justification="left"), gui.Column(kolom9, vertical_alignment="top")]
-    ]
+    tijd_uur=int(lijst[2]/60)
+    tijd_minuut=lijst[2]-(tijd_uur*60)
+    kolom3=[[gui.Text(lijst[1], font = ("Arial, 16"))], [gui.Text(f"{tijd_uur} u {tijd_minuut} min")], [gui.Text(vind_film_via_imdb(lijst[5])['Beschrijving'],size=(30,10))]]
+    kolom6=[[gui.Button('Terug naar films', key='-TERUG-')]]
+    kolom7=[[gui.Button('Vertoning kiezen', key='-KIEZEN-', disabled=True)]]    
+    kolom5=[[gui.Column(kolom6, element_justification="left"),gui.Column(kolom7, element_justification="right")]]
+    kolom4=[[gui.Listbox(info, size=(38, 5), key="-infofilm-", enable_events=True)], [gui.Column(kolom5)]]
+    kolom2=[[gui.Column(kolom3)], [gui.Column(kolom4)]]
 
-    kolom6=[
-        [gui.Column(kolom7, vertical_alignment="top")],
-        [gui.Text("Het gevecht tussen detective Hoffman en Jill Tuck duurt voort. Ondertussen ontmoeten een aantal overleevenden van de spelletjes van Jigsaw, waaronder Dr. Lawrence Gordon, elkaar bij een sessie van Bobby Dagen, die beweert zelf ook een overlevende van Jigsaw te zijn. Maar dan wordt Bobby ontvoerd en zal hij een serie tests moeten afleggen om zijn vrienden en uiteindelijk zijn vrouw te redden.", key='-OMSCHRIJVING-', size=(45,10))]
+    gegevens=lijsturl_naar_lijstimagegui([lijst], x=500, y=500)
+    layout=[[gegevens[0][0], gui.Column(kolom2)]]
+    window = gui.Window("Cinemax", layout,size=(700, 600), element_justification = "center")
+    return window 
 
-    ]
-    layout = [
-        [gui.Column(kolom5, justification="center")],
-        [gui.Column(kolom1, vertical_alignment="top"), gui.Column(kolom2, vertical_alignment="top")],
-        [gui.Frame("",kolom6, key='-FRAME-',border_width=3, visible=False)]#[gui.Column(kolom6, justification="center", size=(200,200))],
-    ]
-    window1 = gui.Window("Cinemax", layout,size=(635, 630), element_justification = "center")
-    return window1
-
-# ------------------ WINDOW 2 ----------------
-def win2():
-    kolom6=[
-        [gui.Text('Geniet van de film!', font=('arial', 15))],
-        [gui.Text(titel, font=('arial', 10))],
-        [gui.Text(f"{datum}", font=('arial', 10))], 
-        [gui.Text(f"om {moment} uur", font=('arial', 10))], 
-        [gui.Text(f"in zaal {zaal}", font=('arial', 10)),], 
-        [gui.Text(f"{DrieD}", font=('arial', 10))],
-        [gui.Button('OK', key='-OK2-', enable_events=True)]
-    ]
-    layout2=[
-        [gui.Column(kolom6, element_justification="center")]
-    ]
-
-    window2= gui.Window("Success!",layout2, size=(220,220))
-    return window2
+# ------------------ WINDOW 3 ----------------
+def win3(lijst):
+    pass
 
 
-# ------------------ VARIABLES WITH FUNCTIONS ----------------
-films=controle_dubbels(films)
-window0=win0()
-window1=win1()
+# ------------------ CUSTOM VARIABLES ----------------
+vandaag=datetime.now()
+vandaag_datum='06/06/2021'#vandaag.strftime("%d/%m/%Y")
+vandaag_tijd=vandaag.strftime("%H:%M")
+vandaag_jaar=vandaag.strftime("%Y")
+vandaag_maand_getal=vandaag.strftime("%M")
+vandaag_week=vandaag.strftime("%W")
+prijs_ticket={'standaard':9,'kids':7,'langspeel':1,'3D':1.5}
+
+allevertoningen=get_vertoning_list() #vb: [7, (1, 'Forrest Gump'), '04/06/2021', '10:00', 'Nee', 7]
+alleverkopen=get_verkoop_list() #vb: [1, 1, 2, 2, 32.0]
+allefilms=get_inventory_list() #vb: [18, 'Alien', 122, 'Ja', 'tt2316204', 'Nee']
+
+titel=[""]
+datum=""
+moment=""
+zaal=""
+DrieD=""
+#TODO: CHECK VARIABLES
+
+
+
+huidigevertoning=[vertoning for vertoning in allevertoningen if vertoning[2]=="06/06/2021"]#vandaag_datum] vb: [7, (1, 'Forrest Gump'), '04/06/2021', '10:00', 'Nee', 7]
+lijstfilms=[(vertoning[1][0],vertoning[1][1]) for vertoning in huidigevertoning] #vb:[(18, 'Alien')]
+films=[i[1] for i in lijstfilms]
+korteinfofilms=[[f[0], f[1], k[2],k[3],k[4]] for k in allefilms for f in lijstfilms if k[0] == f[0] ] #vb: [[18, 'Alien', 122, 'Ja', tt2316204]]
+gegevensfilms=[] #vb: [14, 'Back to the Future', 116, 'Nee', 'https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg', 'tt0088763']
+info=[]
+
+
 
 # ------------------ SHOWTIME ----------------
+window=win0()
 while True:
+    event, values =window.read(timeout=10)
+    if event == None:
+        break
+    else:
+        postersfilms=[vind_film_via_imdb(k[4])['Poster_link'] for k in korteinfofilms] #via imdb-id wordt de link naar de poster gehaald
+        for nummer in range (len(korteinfofilms)):
+            korteinfofilms[nummer].insert(-1,postersfilms[nummer]) #de link naar de poster wordt bij de details van films in vertoning toegevoegd
+        gegevensfilms=controle_dubbels(korteinfofilms) #dubbels worden verwijderd
+        reclame=lijsturl_naar_lijstimagegui(gegevensfilms, x=150,y=150) #posters (in jpg) worden omgebouwd (naar png) en de data wordt als imagegui opgeslagen in de lijst 'posters'
+        break
+window.close()
 
-    event, values=window0.read()
-    event=gui.WIN_CLOSED
-    sleep(1)
+
+window=win1(reclame)
+while True:
+    event, values = window.read()
     if event == gui.WIN_CLOSED:
         break
-    window0.close()
+    if event == "-POSTER1-":
+        window.close()
+        window=win2(gegevensfilms[0])#De eerst film links boven
 
+    if event == "-POSTER2-":
+        window.close()
+        window=win2(gegevensfilms[1])
 
+    if event == "-POSTER3-":
+        window.close()
+        window=win2(gegevensfilms[2])
 
+    if event == "-POSTER4-":
+        window.close()
+        window=win2(gegevensfilms[3])
 
-while True:
-    event, values = window1.read()
-    if event == '-films-':
-        window1["-FRAME-"].update(visible=True)
-        info=[]
-        titel=values['-films-'][0]
-        for i in allevertoningen:
-            if i[1][1]==values['-films-'][0] and i[2]==vandaag_datum:
-                vertoningid=i[0]
-                datum=i[2]
-                moment=i[3]
-                zaal=i[5]
-                DrieD='.' if i[4]=='Nee' else ' in 3D.'
-                info.append(f"{datum} om {moment} uur in zaal {zaal}{DrieD}")
-        
-        window1["-FRAME-"].update(value=titel)
-        imdb=[i[4] for i in gegevensfilms if i[1]==titel]
-        proef=vind_film_via_imdb(imdb[0])
-        omschrijving=proef["Beschrijving"]
-        window1["-OMSCHRIJVING-"].update(value=omschrijving)
+    if event == "-POSTER5-":
+        window.close()
+        window=win2(gegevensfilms[4])
+    if event == "-POSTER6-":
+        window.close()
+        window=win2(gegevensfilms[5])
+    if event == "-POSTER7-":
+        window.close()
+        window=win2(gegevensfilms[6])
+    if event == "-POSTER8-":
+        window.close()
+        window=win2(gegevensfilms[7])
+    if event == "-POSTER9-":
+        window.close()
+        window=win2(gegevensfilms[8])
+    if event == "-POSTER10-":
+        window.close()
+        window=win2(gegevensfilms[9])
 
-        for i in gegevensfilms:
-            if i[1]==titel:
-                duur=i[2]
-        window1["-DUUR-"].update(value=f"{duur} min.")
+    if event == '-KIEZEN-':
+        window.close()
+        window=win3(reclame)
 
-        for i in gegevensfilms:
-            if i[1]==titel:
-                KNT=i[3]
-        if KNT=='Ja':
+    if event =='-TERUG-':
+        window.close()
+        window=win1(reclame)
 
-            window1["-AANTAL_KINDEREN-"].update(visible=False)
-            window1["-SPINKINDEREN-"].update(visible=False)
-        else:
-
-            window1["-AANTAL_KINDEREN-"].update(visible=True)
-            window1["-SPINKINDEREN-"].update(visible=True)
-        window1["-KNT-"].update(text_color='red' if KNT=='Ja' else 'green')
-        window1["-KNT-"].update(value=f"{'KINDEREN NIET TOEGELATEN' if KNT=='Ja' else 'KINDEREN TOEGELATEN'}")
-        window1["-infofilm-"].update(values=info)
-
-
-    if (event == '-SPINVOLWASSENEN-' or event == '-SPINKINDEREN-'):
-        if (values['-SPINVOLWASSENEN-']!='0' or values['-SPINKINDEREN-']!='0'):
-            window1['-KNOP-'].update(disabled=False)
-            prijs= "€ {:.2f}".format((float(values['-SPINVOLWASSENEN-']) * (prijs_ticket['standaard']+prijs_ticket['3D']+prijs_ticket['langspeel']) + float(values['-SPINKINDEREN-']) * (prijs_ticket['standaard']+prijs_ticket['3D']+prijs_ticket['langspeel'])))
-            window1['-PRIJS-'].update(value=prijs)
-        elif (values['-SPINVOLWASSENEN-']=='0' and values['-SPINKINDEREN-']=='0'):# or not values["-infofilm-"] :
-            window1['-KNOP-'].update(disabled=True)
-            window1['-PRIJS-'].update(value="")
-                    
-
-    if  event == '-KNOP-':
-        insert_verkoop_database(vertoningid, values['-SPINKINDEREN-'], values['-SPINVOLWASSENEN-'], prijs)
-        window2=win2()
-        while True:
-            event, values = window2.read()
-            window2.close()
-            break
-
-
-        window1=win1()
-        # window2=win2()
-        # while True:
-        #     event, values = window2.read()
-        #     if event == '-OK2-':
-        #         window1=win1()
-        #         window2.close()
-        #         break
-        #     if event == gui.WIN_CLOSED:
-        #         window1=win1()
-        #         window2.close()
-        #         break
-
-    if event == gui.WIN_CLOSED:
-        break
-
-window1.close()
+    if event == '-infofilm-':
+        window['-KIEZEN-'].update(disabled=False)
+window.close()
